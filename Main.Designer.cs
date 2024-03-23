169d0019049d25e4ccbdac4694cb92c6ce7affe2
Vectorflex <vectorflex@gmail.com>
2024-03-23 10:23:30 +0300

    init

---
+++ Main.Designer.cs
@@ -0,0 +1,243 @@
+﻿using System.Windows.Forms;
+
+namespace TextComparer{
+    partial class MainForm{
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing){
+            if (disposing && (components != null)){
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent(){
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.PanelTopLeft = new System.Windows.Forms.Panel();
+            this.LabelFileRightName = new System.Windows.Forms.Label();
+            this.LabelFileLeftName = new System.Windows.Forms.Label();
+            this.CompareButton = new System.Windows.Forms.Button();
+            this.FileRightButton = new System.Windows.Forms.Button();
+            this.FileLeftButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.RichTextBoxLeft = new System.Windows.Forms.RichTextBox();
+            this.RichTextBoxRight = new System.Windows.Forms.RichTextBox();
+            this.CheckBoxSyncScroll = new System.Windows.Forms.CheckBox();
+            this.RichTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.PanelTopRight = new System.Windows.Forms.Panel();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.LabelResult = new System.Windows.Forms.Label();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.PanelTopLeft.SuspendLayout();
+            this.PanelTopRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelTopLeft
+            // 
+            this.PanelTopLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelTopLeft.Controls.Add(this.LabelFileRightName);
+            this.PanelTopLeft.Controls.Add(this.LabelFileLeftName);
+            this.PanelTopLeft.Controls.Add(this.CompareButton);
+            this.PanelTopLeft.Controls.Add(this.FileRightButton);
+            this.PanelTopLeft.Controls.Add(this.FileLeftButton);
+            this.PanelTopLeft.Location = new System.Drawing.Point(5, 0);
+            this.PanelTopLeft.Name = "PanelTopLeft";
+            this.PanelTopLeft.Size = new System.Drawing.Size(615, 35);
+            this.PanelTopLeft.TabIndex = 0;
+            // 
+            // LabelFileRightName
+            // 
+            this.LabelFileRightName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LabelFileRightName.Location = new System.Drawing.Point(350, 7);
+            this.LabelFileRightName.Name = "LabelFileRightName";
+            this.LabelFileRightName.Size = new System.Drawing.Size(178, 23);
+            this.LabelFileRightName.TabIndex = 4;
+            this.LabelFileRightName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LabelFileLeftName
+            // 
+            this.LabelFileLeftName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LabelFileLeftName.Location = new System.Drawing.Point(84, 7);
+            this.LabelFileLeftName.Name = "LabelFileLeftName";
+            this.LabelFileLeftName.Size = new System.Drawing.Size(178, 23);
+            this.LabelFileLeftName.TabIndex = 3;
+            this.LabelFileLeftName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CompareButton
+            // 
+            this.CompareButton.Enabled = false;
+            this.CompareButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.CompareButton.Location = new System.Drawing.Point(268, 7);
+            this.CompareButton.Name = "CompareButton";
+            this.CompareButton.Size = new System.Drawing.Size(75, 23);
+            this.CompareButton.TabIndex = 2;
+            this.CompareButton.Text = "Compare";
+            this.CompareButton.UseVisualStyleBackColor = true;
+            this.CompareButton.Click += new System.EventHandler(this.CompareButton_Click);
+            // 
+            // FileRightButton
+            // 
+            this.FileRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.FileRightButton.Location = new System.Drawing.Point(534, 7);
+            this.FileRightButton.Name = "FileRightButton";
+            this.FileRightButton.Size = new System.Drawing.Size(75, 23);
+            this.FileRightButton.TabIndex = 1;
+            this.FileRightButton.Text = "File 2";
+            this.FileRightButton.UseVisualStyleBackColor = true;
+            this.FileRightButton.Click += new System.EventHandler(this.FileRightButton_Click);
+            // 
+            // FileLeftButton
+            // 
+            this.FileLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.FileLeftButton.Location = new System.Drawing.Point(3, 7);
+            this.FileLeftButton.Name = "FileLeftButton";
+            this.FileLeftButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.FileLeftButton.Size = new System.Drawing.Size(75, 23);
+            this.FileLeftButton.TabIndex = 0;
+            this.FileLeftButton.Text = "File 1";
+            this.FileLeftButton.UseVisualStyleBackColor = true;
+            this.FileLeftButton.Click += new System.EventHandler(this.FileLeftButton_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.DefaultExt = "txt";
+            this.OpenFileDialog.Filter = "All files(*.*)|*.*|Text files(*.txt)|*.txt|Source files|*.c;*.cpp;*.h;*.cs;*.java" +
+    ";*.js;*.py;*.php;*.asm|HTML|*.html;*.htm|CSS|*.css";
+            // 
+            // RichTextBoxLeft
+            // 
+            this.RichTextBoxLeft.Location = new System.Drawing.Point(5, 38);
+            this.RichTextBoxLeft.Name = "RichTextBoxLeft";
+            this.RichTextBoxLeft.ReadOnly = true;
+            this.RichTextBoxLeft.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.RichTextBoxLeft.Size = new System.Drawing.Size(305, 400);
+            this.RichTextBoxLeft.TabIndex = 1;
+            this.RichTextBoxLeft.Text = "";
+            this.RichTextBoxLeft.VScroll += new System.EventHandler(this.RichTextBoxLeft_VScroll);
+            // 
+            // RichTextBoxRight
+            // 
+            this.RichTextBoxRight.Location = new System.Drawing.Point(314, 38);
+            this.RichTextBoxRight.Name = "RichTextBoxRight";
+            this.RichTextBoxRight.ReadOnly = true;
+            this.RichTextBoxRight.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.RichTextBoxRight.Size = new System.Drawing.Size(305, 400);
+            this.RichTextBoxRight.TabIndex = 2;
+            this.RichTextBoxRight.Text = "";
+            // 
+            // CheckBoxSyncScroll
+            // 
+            this.CheckBoxSyncScroll.AutoSize = true;
+            this.CheckBoxSyncScroll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.CheckBoxSyncScroll.Location = new System.Drawing.Point(261, 444);
+            this.CheckBoxSyncScroll.Name = "CheckBoxSyncScroll";
+            this.CheckBoxSyncScroll.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.CheckBoxSyncScroll.Size = new System.Drawing.Size(88, 17);
+            this.CheckBoxSyncScroll.TabIndex = 3;
+            this.CheckBoxSyncScroll.Text = "Sync scroll";
+            this.CheckBoxSyncScroll.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CheckBoxSyncScroll.UseVisualStyleBackColor = true;
+            // 
+            // RichTextBoxResult
+            // 
+            this.RichTextBoxResult.Location = new System.Drawing.Point(624, 38);
+            this.RichTextBoxResult.Name = "RichTextBoxResult";
+            this.RichTextBoxResult.ReadOnly = true;
+            this.RichTextBoxResult.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.RichTextBoxResult.Size = new System.Drawing.Size(305, 400);
+            this.RichTextBoxResult.TabIndex = 4;
+            this.RichTextBoxResult.Text = "";
+            // 
+            // PanelTopRight
+            // 
+            this.PanelTopRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelTopRight.Controls.Add(this.ButtonSave);
+            this.PanelTopRight.Controls.Add(this.LabelResult);
+            this.PanelTopRight.Location = new System.Drawing.Point(625, 0);
+            this.PanelTopRight.Name = "PanelTopRight";
+            this.PanelTopRight.Size = new System.Drawing.Size(306, 35);
+            this.PanelTopRight.TabIndex = 5;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Enabled = false;
+            this.ButtonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.ButtonSave.Location = new System.Drawing.Point(225, 5);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(75, 23);
+            this.ButtonSave.TabIndex = 6;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            this.ButtonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // LabelResult
+            // 
+            this.LabelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.LabelResult.Location = new System.Drawing.Point(63, 5);
+            this.LabelResult.Name = "LabelResult";
+            this.LabelResult.Size = new System.Drawing.Size(178, 23);
+            this.LabelResult.TabIndex = 5;
+            this.LabelResult.Text = "Result";
+            this.LabelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaveFileDialog
+            // 
+            this.SaveFileDialog.Filter = "All files(*.*)|*.*|Text files(*.txt)|*.txt|Source files|*.c;*.cpp;.h;*.cs;*.java;" +
+    "*.js;*.py;*.php;*.asm|HTML|*.html;*.htm|CSS|*.css";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(938, 471);
+            this.Controls.Add(this.PanelTopRight);
+            this.Controls.Add(this.RichTextBoxResult);
+            this.Controls.Add(this.CheckBoxSyncScroll);
+            this.Controls.Add(this.RichTextBoxRight);
+            this.Controls.Add(this.RichTextBoxLeft);
+            this.Controls.Add(this.PanelTopLeft);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(954, 510);
+            this.Name = "MainForm";
+            this.Text = "TextComparer";
+            this.PanelTopLeft.ResumeLayout(false);
+            this.PanelTopRight.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelTopLeft;
+        private System.Windows.Forms.Button FileRightButton;
+        private System.Windows.Forms.Button FileLeftButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.RichTextBox RichTextBoxLeft;
+        private System.Windows.Forms.RichTextBox RichTextBoxRight;
+        private Button CompareButton;
+        private CheckBox CheckBoxSyncScroll;
+        private Label LabelFileLeftName;
+        private Label LabelFileRightName;
+        private RichTextBox RichTextBoxResult;
+        private Panel PanelTopRight;
+        private Label LabelResult;
+        private Button ButtonSave;
+        private SaveFileDialog SaveFileDialog;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using DiffMatchPatch;
+using System.Runtime.InteropServices;
+
+
+namespace TextComparer{
+    public partial class MainForm : Form {
+        //Библиотеки для одновременной прокрутки
+        [DllImport("User32.dll")]
+        public extern static int GetScrollPos(IntPtr hWnd, int nBar);
+
+        [DllImport("User32.dll")]
+        public extern static int SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+        public enum ScrollBarType : uint{
+            SbHorz = 0,
+            SbVert = 1,
+            SbCtl = 2,
+            SbBoth = 3
+        }
+
+        public enum Message : uint{
+            WM_VSCROLL = 0x0115,
+            WM_HSCROLL = 0x0114,
+        }
+
+        public enum ScrollBarCommands : uint{
+            SB_THUMBPOSITION = 4
+        }
+
+        bool leftFileIsLoaded = false;
+        bool rightFileIsLoaded = false;
+
+        //Объект diff_match_patch
+        diff_match_patch DIFF = new diff_match_patch();
+
+        //Список различий
+        List<Diff> diffs;
+
+        //Список частей в RichTextBox для подсветки
+        List<Chunk> chunklist1;
+        List<Chunk> chunklist2;
+
+        //Список различающихся частей для сохранения в файл
+        List<string> outlist1 = new List<string>();
+        List<string> outlist2 = new List<string>();
+
+        //Цвета подсветки
+        Color[] colors1 = new Color[3] { Color.LightGreen, Color.LightSalmon, Color.White };
+        Color[] colors2 = new Color[3] { Color.LightSalmon, Color.LightGreen, Color.White };
+
+        int firstFileLength = 0;
+        int secondFileLength = 0;
+
+        int firstFileDiffsCount = 0; //Количество различий в первом файле
+        int secondFileDiffsCount = 0; //Количество различий во втором файле
+        int diffsCount = 0; //Общее количество различий
+
+        int firstFileCharDiffsCount = 0; //Количество различий по символам в первом файле
+        int secondFileCharDiffsCount = 0; //Количество различий по символам во втором файле
+        int charDiffsCount = 0; //Общее количество различий по символам
+
+        string resultStr; //Строка результата для отображение в RichTextBox сохранения файла
+
+
+        public struct Chunk{
+            public int startpos;
+            public int length;
+            public Color BackColor;
+        }
+
+        public MainForm(){
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; //Запретить изменение размера формы
+        }
+
+        private void FileLeftButton_Click(object sender, EventArgs e){
+            if (OpenFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            //Получаем выбранный файл
+            string filename = OpenFileDialog.FileName;
+
+            FileInfo fileinfo = new FileInfo(filename);
+            
+            if (fileinfo.Length == 0) {
+                MessageBox.Show(
+                             "File is empty",
+                             "Error",
+                             MessageBoxButtons.OK,
+                             MessageBoxIcon.Error);
+                return;
+            }
+
+            //Читаем файл в текстовое поле
+            string txt = File.ReadAllText(filename);
+            string handledTxt = DeleteExcessSpaceCharacters(txt);
+
+            if (handledTxt.Length == 0) {
+                MessageBox.Show(
+                            "File is empty",
+                            "Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                return;
+            }
+            
+            RichTextBoxLeft.Text = handledTxt;
+            firstFileLength = RichTextBoxLeft.Text.Length;
+            leftFileIsLoaded = true;
+
+
+            string[] strParts = filename.Split('\\');
+            filename = strParts[strParts.Length - 1];
+            LabelFileLeftName.Text = TrunkString(filename);
+
+            RichTextBoxResult.Text = "";
+
+            //Активация кнопки Сравнить, если оба файла загружены
+            if (CanCompareFiles(leftFileIsLoaded, rightFileIsLoaded)) {
+                CompareButton.Enabled = true;
+            }
+
+        }
+
+        private void FileRightButton_Click(object sender, EventArgs e){
+            if (OpenFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            //Получаем выбранный файл
+            string filename = OpenFileDialog.FileName;
+
+            FileInfo fileinfo = new FileInfo(filename);
+            if (fileinfo.Length == 0){
+                MessageBox.Show(
+                             "File is empty",
+                             "Error",
+                             MessageBoxButtons.OK,
+                             MessageBoxIcon.Error);
+                return;
+            }
+
+            //Читаем файл в текстовое поле
+            string txt = File.ReadAllText(filename);
+            string handledTxt = DeleteExcessSpaceCharacters(txt);
+
+            if (handledTxt.Length == 0){
+                MessageBox.Show(
+                            "File is empty",
+                            "Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                return;
+            }
+
+            RichTextBoxRight.Text = handledTxt;
+            secondFileLength = RichTextBoxRight.Text.Length;
+            rightFileIsLoaded = true;
+
+            string[] strParts = filename.Split('\\');
+            filename = strParts[strParts.Length - 1];
+            LabelFileRightName.Text = TrunkString(filename);
+
+            RichTextBoxResult.Text = "";
+
+            //Активация кнопки Сравнить, если оба файла загружены
+            if (CanCompareFiles(leftFileIsLoaded, rightFileIsLoaded)){
+                CompareButton.Enabled = true;
+            }
+
+        }
+
+        private void RichTextBoxLeft_VScroll(object sender, EventArgs e){
+            if (!CheckBoxSyncScroll.Checked) return;
+
+            int nPos = GetScrollPos(RichTextBoxLeft.Handle, (int)ScrollBarType.SbVert);
+            nPos <<= 16;
+            uint wParam = (uint)ScrollBarCommands.SB_THUMBPOSITION | (uint)nPos;
+            SendMessage(RichTextBoxRight.Handle, (int)Message.WM_VSCROLL, new IntPtr(wParam), new IntPtr(0));
+        }
+
+        private bool CanCompareFiles(bool leftFileIsLoaded, bool rightFileIsLoaded) {
+            return leftFileIsLoaded && rightFileIsLoaded;
+        }
+
+        //Сравнение файлов
+        private void CompareButton_Click(object sender, EventArgs e){
+            outlist1.Clear();
+            outlist2.Clear();
+
+            firstFileCharDiffsCount = 0;
+            secondFileCharDiffsCount = 0;
+
+            diffs = DIFF.diff_main(RichTextBoxLeft.Text, RichTextBoxRight.Text);
+            DIFF.diff_cleanupSemanticLossless(diffs);
+
+            //Получить отличия в список
+            chunklist1 = CollectChunks(RichTextBoxLeft);
+            chunklist2 = CollectChunks(RichTextBoxRight);
+
+            //Подсветка отличий
+            PaintChunks(RichTextBoxLeft, chunklist1, ref outlist1);
+            PaintChunks(RichTextBoxRight, chunklist2, ref outlist2);
+
+            RichTextBoxLeft.SelectionLength = 0;
+            RichTextBoxRight.SelectionLength = 0;
+
+            firstFileDiffsCount = outlist1.Count; //Количество различий в первом файле
+            secondFileDiffsCount = outlist2.Count; //Количество различий во втором файле
+            diffsCount = firstFileDiffsCount + secondFileDiffsCount; // Общее количество различий
+
+            if (diffsCount > 0){
+                    resultStr = $"First file diffs count: {firstFileDiffsCount}\n" +
+                                $"Second file diffs count: {secondFileDiffsCount}\n" +
+                                $"Total diffs count: {diffsCount}\n\n";
+
+                string firstFileDiffs = "";
+                string secondFileDiffs = "";
+
+                foreach (string str in outlist1){
+                    firstFileDiffs += str + "\n";
+                    firstFileCharDiffsCount += str.Length;
+                }
+                 
+                foreach (string str in outlist2){
+                    secondFileDiffs += str + "\n";
+                    secondFileCharDiffsCount += str.Length;
+                }
+
+                charDiffsCount = firstFileCharDiffsCount + secondFileCharDiffsCount;
+                //Метрика, которая определяет насколько отличаются файлы
+                float diffIndex = (firstFileCharDiffsCount + secondFileCharDiffsCount) / (float)(firstFileLength + secondFileLength);
+                float simDistance = 1 - diffIndex;
+                resultStr += $"First file by char diffs count: {firstFileCharDiffsCount}\n" +
+                             $"Second file by char diffs count: {secondFileCharDiffsCount}\n" +
+                             $"Total diffs by char count: {charDiffsCount}\n" +
+                             $"Diff index (min -  0, max - 1): {diffIndex}\n" +
+                             $"Sim index (min -  0, max - 1): {simDistance}\n\n";
+
+                if (firstFileCharDiffsCount > 0) {
+                    resultStr += $"First file diffs:\n{firstFileDiffs}\n\n";
+                }
+
+                if (secondFileCharDiffsCount > 0){
+                    resultStr += $"Second file diffs:\n{secondFileDiffs}";
+                }
+
+                ButtonSave.Enabled = true;
+            }else {
+                resultStr = "No diffs";
+                ButtonSave.Enabled = false;
+            }
+
+            RichTextBoxResult.Text = resultStr;
+        }
+
+        //Получить части текста
+       private List<Chunk> CollectChunks(RichTextBox RTB){
+            RTB.Text = "";
+            List<Chunk> chunkList = new List<Chunk>();
+            foreach (Diff diff in diffs){
+                if (RTB == RichTextBoxRight && diff.operation == Operation.DELETE) continue; 
+                if (RTB == RichTextBoxLeft && diff.operation == Operation.INSERT) continue; 
+
+                Chunk chunk = new Chunk();
+                int length = RTB.TextLength;
+                RTB.AppendText(diff.text);
+                chunk.startpos = length;
+                chunk.length = diff.text.Length;
+                chunk.BackColor = (RTB == RichTextBoxLeft) ? colors1[(int)diff.operation]
+                                           : colors2[(int)diff.operation];
+                chunkList.Add(chunk);
+         
+            }
+            return chunkList;
+        }
+
+        //Подсветка различий
+       private void PaintChunks(RichTextBox RTB, List<Chunk> chunks, ref List<string> outlist){
+            foreach (Chunk chunk in chunks){
+                RTB.Select(chunk.startpos, chunk.length);
+                RTB.SelectionBackColor = chunk.BackColor;
+                
+                //Выделение различий
+                if (chunk.BackColor == Color.LightGreen || chunk.BackColor == Color.LightSalmon) {
+                    outlist.Add(RTB.SelectedText);
+                    }
+                }
+            }
+
+
+
+        private void ButtonSave_Click(object sender, EventArgs e){
+            if (SaveFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            
+            //Получаем название файла
+            string filename = SaveFileDialog.FileName;
+            //Сохраняем файл
+            System.IO.File.WriteAllText(filename, resultStr);
+        }
+
+        //Обрезка строки
+        private string TrunkString(string str, int maxCount = 18){
+            return str.Length <= maxCount ? str : str.Substring(0, maxCount - 4) + "...";
+        }
+
+        //Удаление лишних пробельных символов
+        private string DeleteExcessSpaceCharacters(string str){
+            string new_str = System.Text.RegularExpressions.Regex.Replace(str, @"[\n]+", "\n");
+            new_str = System.Text.RegularExpressions.Regex.Replace(new_str, @"[\t]+", "\t");
+            new_str = System.Text.RegularExpressions.Regex.Replace(new_str, @"[ ]+", " ");
+
+            return new_str.Trim();
+        }
+
+    }
+}
+    
+
